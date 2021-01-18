7f3ed8667d79ca3c9cdf3c1a73cc0dbb029d09a4
Nug <hugues.levasseur@arrobe.fr>
2021-01-18 13:49:18 +0100

    Ajout de l'oiseau

--- script/flappybird.js
+++ script/flappybird.js
@@ -7,6 +7,15 @@ var config = {
     update: update     // Fonction appelée 60 fois par seconde
   },
   parent: 'flappyBird', // Affiche le jeu dans le div id="flappyBird"
+  physics: {
+    default: 'arcade', // Permet d'appliquer un set de mouvements aux objets
+    arcade: {
+      gravity: {
+        y: 0
+      },
+    },
+  },
+  backgroundColor: '#71c5cf', // Ciel bleu
 };
 
 // Variables globales
@@ -14,10 +23,19 @@ var game = new Phaser.Game(config);
 
 function preload () {
   // C'est là qu'on vas charger les images et les sons
+  this.load.image('bird', 'img/bird.png');
 }
 function create () {
   // Ici on vas initialiser les variables, l'affichage ...
+
+  piaf = this.physics.add.sprite(100, 245, 'bird'); // Affiche 'bird' en x=100 y=245
+  piaf.body.gravity.y = 1000; // Ajoute jusqu'a 1000 px / frame a la coordonnée y des objets
 }
 function update () {
   // C'est la boucle principale du jeu
+
+  // Si l'oiseau quite l'écran
+  if (piaf.y < 0 || piaf.y > 490) {
+    this.scene.restart(); // On redémare
+  }
 }
