e1d1165b857b5abc15b8c24db29f6a120cf5b170
evan <evan.adam@yahoo.fr>
2021-02-19 18:08:20 +0100

    Add gameManager squeleton

--- class/GameManager.js
+++ class/GameManager.js
@@ -1,7 +1,35 @@
-class GameManager
+class GameManager extends Phaser.Scene
 {
-    constructor()
+    constructor ()
+    {
+        super();
+    }
+
+    preload ()
     {
 
     }
-} ;
+    create ()
+    {
+
+    }
+
+    update ()
+    {
+
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: window.innerWidth  * 0.9,
+    height: window.innerHeight * 0.9,
+    pixelArt: true,
+    physics: {
+        default: 'arcade',
+        fps: 30
+    },
+    scene: [ SceneMain ]
+};
+const game = new Phaser.Game(config);
